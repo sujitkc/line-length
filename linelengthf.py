362c2abd285a24d22da0ccde1d9c6352da968239
sujitkc <sujitkc@gmail.com>
2021-07-30 19:01:02 +0530

    The common parts of loop1 and loop2 have been merged to create a parameterised loop function. make_loop function creates the two variants based on the parameter function update1 or update2 which capture the distinct parts of the two loop functions.

--- linelengthf.py
+++ linelengthf.py
@@ -2,99 +2,75 @@ def add_word(line, words, MaxLength):
   newline = line + words[0]
   if(len(newline) == MaxLength):
     return newline, words[1:]
-  elif(len(newline) < MaxLength):
+  else:
     if(len(words) > 1):
       return newline + " ", words[1:]
     else:
       return newline, []
-  else:
-    raise Exception("add_word: resultant line is longer than MaxLength: "\
-      + newline)
 
 def is_addable(line, word, MaxLength):
   return len(line + word) <= MaxLength
 
 def hyphenate(line, words, MaxLength):
   l = MaxLength - len(line) - 1
-  if(l == 0):
-    raise Exception("Hyphenate can't proceed!")
   word = words[0]
-  w1 = word[:l] + "-"
-  w2 = word[l:]
+  w1, w2 = word[:l] + "-", word[l:]
   newwords = [w1, w2]
   newwords.extend(words[1:])
   return newwords
 
-'''
-This loop starts adding the subwords of a hyphenated word from the
-next line. Once it is seen the the next word can't be inserted into
-the current line in its entirety, the current line is added to the
-list of completed lines. Then looping starts just like start.
-'''
-def loop1(lines, line, words, MaxLength):
-  if(words == []):
-    if(line != ""):
-      return lines + [line]
-    return lines
-  if(is_addable(line, words[0], MaxLength)):
-    newline, newwords = add_word(line, words, MaxLength)
-    if(len(newline) == MaxLength):
-      return loop1(lines + [newline], "", newwords, MaxLength)
-    else:
-      return loop1(lines, newline, newwords, MaxLength)
-  else:
-    if(line == ""):
-      newwords = hyphenate(line, words, MaxLength)
-      return loop1(lines, line, newwords, MaxLength)
-    else:
-      return loop1(lines + [line], "", words, MaxLength)
-
-def loop2(lines, line, words, MaxLength):
-  if(words == []):
-    if(line != ""):
-      return lines + [line]
-    return lines
-  if(is_addable(line, words[0], MaxLength)):
-    newline, newwords = add_word(line, words, MaxLength)
-    if(len(newline) == MaxLength):
-      return loop2(lines + [newline], "", newwords, MaxLength)
-    else:
-      return loop2(lines, newline, newwords, MaxLength)
-  else:
+def update1(lines, line, words, MaxLength):
+  if(line == ""):
     newwords = hyphenate(line, words, MaxLength)
-    return loop2(lines, line, newwords, MaxLength)
+    return loop(lines, line, newwords, MaxLength)
+  else:
+    return loop(lines + [line], "", words, MaxLength)
+
+def update2(lines, line, words, MaxLength):
+  newwords = hyphenate(line, words, MaxLength)
+  return loop(lines, line, newwords, MaxLength)
+
+def make_loop(update):
+  def loop(lines, line, words, MaxLength):
+    if(words == []):
+      if(line != ""):
+        return lines + [line]
+      return lines
+    if(is_addable(line, words[0], MaxLength)):
+      newline, newwords = add_word(line, words, MaxLength)
+      if(len(newline) == MaxLength):
+        return loop(lines + [newline], "", newwords, MaxLength)
+      else:
+        return loop(lines, newline, newwords, MaxLength)
+    else:
+      return update(lines, line, words, MaxLength)
+
+  return loop
+
+loop = make_loop(update2)
 
 def compute_lines(text, MaxLength):
   words = text.split(" ")
-  return loop2([], "", words, MaxLength)
+  return loop([], "", words, MaxLength)
 
 def t_add_word_1():
   line = "Hello "
   words = ["World", "and", "others"]
-  print(add_word(line, words, 15))
+  print(add_word(line, words, 10))
 
 def t_hyphenate_1():
   line = "Hello "
   words = ["Worldand", "others"]
   print(hyphenate(line, words, 10)) 
 
-def t_loop1_1():
+def t_loop_1():
   words = ["hello", "hekjhsfsdhfsebwetrewthewbrtewewtrewbrt"]
   print("words = ", str(words))
-  print(loop1([], "", words, 10))
-
-def t_loop2_1():
-  words = ["hello", "hekjhsfsdhfsebwetrewthewbrtewewtrewbrt"]
-  print("words = ", str(words))
-  print(loop2([], "", words, 10))
+  print(loop([], "", words, 10))
 
 def t_compute_lines_1():
   text = "hello hekjhsfsdhfsebwetrewthewbrtewewtrewbrt"
-  print(compute_lines(text, 15))
+  print(compute_lines(text, 10))
   
 if __name__ == "__main__":
-  #t_add_word_1()
-  #t_hyphenate_1()
-  t_loop1_1()
-  t_loop2_1()
   t_compute_lines_1()
